b352643e4fd1f7bba5bdf5273ab29fba2b41da08
Riki-Toro <rikitoro22@gmail.com>
2021-08-27 19:49:08 +0700

    event

---
+++ jQuery-FrameworkIndonesia/3.event/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('h1').css('textTransform', 'uppercase');
+    $('hr').css({
+        'borderWidth': '2px',
+        'backgroundColor': 'black'
+    });
+
+
+});
