491d058f87ee9a97ca66b71bea3a799ffdb825cb
Riki-Toro <rikitoro22@gmail.com>
2021-08-28 11:39:29 +0700

    selesai efek fading

--- jQuery-FrameworkIndonesia/4.efek/script-dasar-fading.js
+++ jQuery-FrameworkIndonesia/4.efek/script-dasar-fading.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('h1').css('textTransform', 'uppercase')
 
     // efek dasar jQuery
+    // hide()
     $('#hide').click(function() {
         /* parameter efek :
         1.time/waktu ms = 1000, 2000, 3000, dst 
@@ -11,15 +12,42 @@ $(document).ready(function() {
         4. 'fast' */ 
         $('.kotak').hide(3000);
     });
+
+    // show()
     $('#show').click(function() {
         $('.kotak').show('fast');
     });
 
 
+
     // efek fading
+    // fadeOut()
     $('#fadeout').click(function() {
-        
-    })
+        $('.box1').fadeOut();
+        $('.box2').fadeOut(2000);
+        $('.box3').fadeOut(4000);
+    });
+
+    // fadeIn()
+    $('#fadein').click(function() {
+        $('.box1').fadeIn();
+        $('.box2').fadeIn(2000);
+        $('.box3').fadeIn(4000);
+    });
+
+    // fadeToggle()
+    $('#fadetoggle').click(function() {
+        $('.box1').fadeToggle();
+        $('.box2').fadeToggle(1000);
+        $('.box3').fadeToggle(2000);
+    });
+
+    // fadeTo
+    $('#fadeto').click(function() {
+        $('.box1').fadeTo(0, .2);
+        $('.box2').fadeTo(1000, .5);
+        $('.box3').fadeTo(2000, .7);
+    });
 
 
 });
