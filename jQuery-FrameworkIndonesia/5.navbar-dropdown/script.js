91f2a4dcf501a700a3e5d52ed263a57f50796372
Riki-Toro <rikitoro22@gmail.com>
2021-08-29 18:35:12 +0700

    selesai membuat navbar dropdown

--- jQuery-FrameworkIndonesia/5.navbar-dropdown/script.js
+++ jQuery-FrameworkIndonesia/5.navbar-dropdown/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
-    $('h1').css('textTransform', 'uppercase');
+    $('h1').css({
+        'textTransform': 'uppercase',
+        'textAlign': 'center'
+    });
+
+    $('.navbar li').hover(function() {
+        $(this).find('.dropdown').slideToggle(1000);
+    });
 
 });
