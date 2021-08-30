917c8cd715f41bbccbda33ddaf363e5c50621962
Riki-Toro <rikitoro22@gmail.com>
2021-08-30 17:31:26 +0700

    update

--- jQuery-FrameworkIndonesia/6.show-hide-password-bootstrap/script.js
+++ jQuery-FrameworkIndonesia/6.show-hide-password-bootstrap/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
+    $('h1').css({
+        'textAlign': 'center'
+    });
+
+    $('.container').css({
+        'border': '1px solid black',
+        'padding': '50px 30px',
+        'margin': '50px auto',
+        'width': '500px',
+        'borderRadius': '10px'
+    });
 });
