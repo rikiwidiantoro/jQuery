8a5679af583aef7f69e7c1b4e0b89e7add5b64cc
Riki-Toro <rikitoro22@gmail.com>
2021-08-31 20:57:51 +0700

    ganti

--- jQuery-FrameworkIndonesia/7.modal-dialog/script.js
+++ jQuery-FrameworkIndonesia/7.modal-dialog/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $('#tombol').click(function() {
         $('#modal-box').fadeIn(1500);
+        $('#bg').fadeIn(1500);
     });
     $('#close').click(function() {
         $('#modal-box').fadeOut(1000);
+        $('#bg').fadeOut(1000);
     });
 });
