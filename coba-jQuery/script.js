32e4f1e0ecec097d7872e6ab4856c48c38bfefc7
Riki-Toro <rikitoro22@gmail.com>
2021-09-24 18:39:26 +0700

    button jquery

--- coba-jQuery/script.js
+++ coba-jQuery/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
     let nama = 'Riki Widiantoro';
+    let namaGanti = 'Eren Yeager';
 
     $('h3').css('textAlign', 'center');
     $('#nama').text(nama);
 
-    $('button').click(function() {
+    $('.klik').click(function() {
         $('.box').fadeToggle(2000);
     });
 
     $('header span').click(function() {
         alert('Anda berhasil Keluar!');
+    });
+
+    $('.ganti').click(function() {
+        $('#nama').text()
     });
 
 })
