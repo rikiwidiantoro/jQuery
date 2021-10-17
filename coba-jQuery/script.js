66e041cd39b8a59bd7080f5e070ad6f3c00c36fc
Riki-Toro <rikitoro22@gmail.com>
2021-10-17 17:49:48 +0700

    tdk jadi

--- coba-jQuery/script.js
+++ coba-jQuery/script.js
@@ -34,6 +34,7 @@ $(document).ready(function() {
                 'backgroundColor': 'Blue',
                 'color': 'white'
             });
+            console.log(this);
         });
 
         $('#bgk').dblclick(function() {
